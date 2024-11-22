2c6b286e5feba26b3ac4e303a040d4369c74d768
712 <khamidullayew@gmail.com>
2024-11-22 17:24:36 +0500

    done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    colors: {
+      1: " #ff9846",
+      2: "#f2f2f2",
+      3: "#111",
+      4: "#a4acad",
+      5: "#f46d40",
+      7: "#f2f2f2",
+      8: " #fff",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+};
